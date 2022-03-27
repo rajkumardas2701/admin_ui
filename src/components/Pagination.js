212bb5663cd916874d6e8513610ef818e087b40d
rajkumardas2701 <rajkumardas2701@gmail.com>
2022-03-27 14:35:23 +0530

    add logic to change user's list on next and prev button press

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,6 +7,7 @@ import '../styles/Pagination.css';
 const Pagination = ({ users, setPaginatedUsers }) => {
   const [pages, setPages] = useState([]);
   const [startPage, setStartPage] = useState(0);
+  const [activePage, setActivePage] = useState(1);
   useEffect(() => {
     fetchPages(users, setPages, startPage);
   }, [users, startPage]);
@@ -14,17 +15,22 @@ const Pagination = ({ users, setPaginatedUsers }) => {
   const handlePageClick = (event) => {
     event.preventDefault();
     fetchPaginatedUsers(setPaginatedUsers, users, (Number(event.target.id) - 1));
+    setActivePage(Number(event.target.id));
   };
 
   const handlePrevButton = () => {
     if (startPage > 0) {
       setStartPage(startPage - 1);
+      fetchPaginatedUsers(setPaginatedUsers, users, startPage - 1);
+      setActivePage(startPage);
     }
   };
 
   const handleNextButton = () => {
     if (startPage + 2 <= pages.length) {
       setStartPage(startPage + 1);
+      fetchPaginatedUsers(setPaginatedUsers, users, startPage + 1);
+      setActivePage(startPage + 2);
     }
   };
 
@@ -35,10 +41,12 @@ const Pagination = ({ users, setPaginatedUsers }) => {
         <div className="pagination-main">
           <button type="button" onClick={handlePrevButton} disabled={startPage === 0}>Prev</button>
           {
-          pages.map((page) => <button type="button" key={page} className="pagination-item" id={page} onClick={handlePageClick}>{page}</button>)
+          pages.map(
+            (page) => <button type="button" key={page} className={`pagination-item ${activePage === page ? 'active-page' : ''}`} id={page} onClick={handlePageClick}>{page}</button>,
+          )
       }
           <button type="button" onClick={handleNextButton} disabled={startPage + 3 === Math.ceil(users.length / 10)}>Next</button>
-          {/* {console.log(startPage, Math.ceil(users.length / 10))} */}
+          {console.log(startPage)}
         </div>
         )
       }
--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,15 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import '../styles/Users.css';
 import PaginationContext from '../context/PaginationContext';
 
 const Users = () => {
   const users = useContext(PaginationContext);
+  const [selectedUsers, setSelectedUser] = useState([]);
+  // const [selectAllChecked, setSelectAllChecked] = useState(false);
   // console.log(users);
+  const handleClickAll = () => {
+    // setSelectAllChecked(event.target.checked);
+    setSelectedUser(users.map((user) => user.id));
+  };
+  console.log(selectedUsers);
+  const handleSingleUser = (event) => {
+    // console.log(event.target.parentElement.parentElement.id);
+    setSelectedUser(selectedUsers.filter(
+      (user) => user.id !== event.target.parentElement.parentElement.id,
+    ));
+  };
   return (
     <div>
       <table className="users-table">
         <tbody>
           <tr>
+            <th>
+              <input type="checkbox" onChange={handleClickAll} />
+            </th>
             <th>
               Name
             </th>
@@ -25,7 +41,8 @@ const Users = () => {
           </tr>
           {
             users.map((user) => (
-              <tr key={user.id}>
+              <tr key={user.id} id={user.id}>
+                <td><input type="checkbox" checked={selectedUsers.includes(user.id)} onChange={handleSingleUser} /></td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>{user.role}</td>
@@ -35,6 +52,7 @@ const Users = () => {
           }
         </tbody>
       </table>
+      <button type="button">Delete selected user(s)</button>
     </div>
   );
 };
